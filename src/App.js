da555017bdfee0459fd3a161da88a5a3f86b9181
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2020-07-01 10:49:15 -0300

    Adding bootstrap

--- src/App.js
+++ src/App.js
@@ -9,38 +9,33 @@ const App = () => {
           <ul>
             <li>
               <Link to='/sign-in'>Sign in</Link>
-            </li>
-            <li>
               <Link to='/sign-up'>Sign up</Link>
-            </li>
-            <li>
               <Link to='/manage/links/create'>Create Link</Link>
-            </li>
-            <li>
               <Link to='/manage/links/edit'>Edit Link</Link>
-            </li>
-            <li>
               <Link to='/manage/links'>Links</Link>
             </li>
           </ul>
         </nav>
         <Switch>
-          <Route path='/'>
+          <Route exact path='/'>
             <h1>Home</h1>
           </Route>
-          <Route path='/sign-in'>
+          <Route exact path='/home'>
+            <h1>Home</h1>
+          </Route>
+          <Route exact path='/sign-in'>
             <h1>Sign In</h1>
           </Route>
-          <Route path='/sign-up'>
+          <Route exact path='/sign-up'>
             <h1>Sign Up</h1>
           </Route>
-          <Route path='/manage/links/create'>
+          <Route exact path='/manage/links/create'>
             <h1>Create Link</h1>
           </Route>
-          <Route path='/manage/links/edit'>
+          <Route exact path='/manage/links/edit'>
             <h1>Edit Link</h1>
           </Route>
-          <Route path='/manage/links'>
+          <Route exact path='/manage/links'>
             <h1>Links</h1>
           </Route>
         </Switch>
