79d9d3e833974703196000663a25a9536ba6ae48
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2020-07-01 14:23:50 -0300

    Redux and axios sign up

--- src/store.js
+++ src/store.js
@@ -2,9 +2,11 @@ import { createStore, applyMiddleware, combineReducers } from 'redux'
 import ReduxPromise from 'redux-promise'
 
 import signInReducer from './pages/SignIn/SignInReducer'
+import signUpReducer from './pages/SignUp/SignUpReducer'
 
 const reducers = combineReducers({
-  signIn: signInReducer
+  signIn: signInReducer,
+  signUp: signUpReducer
 })
 
 const store = createStore(reducers, applyMiddleware(ReduxPromise))
