906974a09cf31e430897b0c70c60c9d24fe5549e
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2020-07-01 11:07:23 -0300

    Adding Now UI Kit

--- src/App.js
+++ src/App.js
@@ -9,9 +9,17 @@ const App = () => {
           <ul>
             <li>
               <Link to='/sign-in'>Sign in</Link>
+            </li>
+            <li>
               <Link to='/sign-up'>Sign up</Link>
+            </li>
+            <li>
               <Link to='/manage/links/create'>Create Link</Link>
+            </li>
+            <li>
               <Link to='/manage/links/edit'>Edit Link</Link>
+            </li>
+            <li>
               <Link to='/manage/links'>Links</Link>
             </li>
           </ul>
