49108144de3718b35e8313f1d25dc4a77b52c40b
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2020-07-01 12:14:54 -0300

    Manage pages

--- src/App.js
+++ src/App.js
@@ -1,38 +1,39 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
 
+import Home from './pages/Home'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
+import ManageLinks from './pages/Manage/Links'
+import CreateLink from './pages/Manage/Create'
+import EditLink from './pages/Manage/Edit'
 
 const App = () => {
   return (
     <BrowserRouter>
       <div>
         <nav>
-          <ul>
-            <li>
+          <ul className="list-group list-group-horizontal">
+            <li className="list-group-item">
               <Link to='/sign-in'>Sign in</Link>
             </li>
-            <li>
+            <li className="list-group-item">
               <Link to='/sign-up'>Sign up</Link>
             </li>
-            <li>
+            <li className="list-group-item">
               <Link to='/manage/links/create'>Create Link</Link>
             </li>
-            <li>
+            <li className="list-group-item">
               <Link to='/manage/links/edit'>Edit Link</Link>
             </li>
-            <li>
+            <li className="list-group-item">
               <Link to='/manage/links'>Links</Link>
             </li>
           </ul>
         </nav>
         <Switch>
           <Route exact path='/'>
-            <h1>Home</h1>
-          </Route>
-          <Route exact path='/home'>
-            <h1>Home</h1>
+            <Home />
           </Route>
           <Route exact path='/sign-in'>
             <SignIn />
@@ -41,13 +42,13 @@ const App = () => {
             <SignUp />
           </Route>
           <Route exact path='/manage/links/create'>
-            <h1>Create Link</h1>
+            <CreateLink />
           </Route>
           <Route exact path='/manage/links/edit'>
-            <h1>Edit Link</h1>
+            <EditLink />
           </Route>
           <Route exact path='/manage/links'>
-            <h1>Links</h1>
+            <ManageLinks />
           </Route>
         </Switch>
       </div>
